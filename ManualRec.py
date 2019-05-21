0cef7d9e9291c279ac5df0eef3222c3b0d526a5f
SweJon <46954315+SweJon@users.noreply.github.com>
2019-05-22 00:15:49 +0200

    Kommentarer osv
    La till lite kommentarer för bättre förståelse för tanken med koden och tog väck ManualRec2 då det bara var kopierade kod i rent testsyfte.

--- ManualRec.py
+++ ManualRec.py
@@ -3,14 +3,14 @@ matplotlib.__version__'3.0.3'
 import PIL
 PIL.__version__'6.0.0'
 import scipy
-scipy.__version__'1.2.1'
+scipy.__version__'1.2.1' #importerar de funktioner som behövs för att det ska funka
 
 
 
 
 from matplotlib import image as imgimage = img.imread('./banana1.jpg')
 image.shape # Det manuella programmet som ska försöka att på ett simplare sätt avläsa bilder som att hitta bildens dominanta färg
-(355, 355, 3)
+(355, 355, 3) # Antalet pixlar bilden har och en 3:a som representerar att de 3 olika typerna av färgvärden (rgb)
 
 %matplotlib inlinefrom matplotlib import pyplot as pltplt.imshow(image)
 plt.show()
@@ -25,10 +25,10 @@ g.append(temp_g)
 b.append(temp_b)
 
 
-from mpl_toolkits.mplot3d import Axes3Dfig = plt.figure() #Koden för att 3d visualisera bilden
+from mpl_toolkits.mplot3d import Axes3Dfig = plt.figure() #Koden för att 3d visualisera bildens färger (en 3d graf som visar r,g och b värdena på varje axel för att visualisera den vanligaste färgen)
 ax = Axes3D(fig)
 ax.scatter(r, g, b)
-plt.show()
+plt.show() #"Kommandot" som visar grafen
 
 import pandas as pddf = pd.DataFrame({'red': r,
 'blue': b,
@@ -50,9 +50,9 @@ print(cluster_centers)
 colors = []r_std, g_std, b_std = df[['red', 'green', 'blue']].std()for cluster_center in cluster_centers:
 scaled_r, scaled_g, scaled_b = cluster_center
 colors.append((
-scaled_r * r_std / 255,
+scaled_r * r_std / 255, # Delar färgen med 255 för att ge ett rgb-värde mellan 0 till 1
 scaled_g * g_std / 255,
 scaled_b * b_std / 255
 ))
-plt.imshow([colors])
+plt.imshow([colors]) # Tar 0 till 1 värderna och genererar bildens "medelfärg"
 plt.show()
--- Web_crawler.py
+++ Web_crawler.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup #tilläg som används för att läsa av hemsidor
 def image_spider(max_pages): #funktion som tar upp bildernas url för att ladda ner de
     page = 1
     while page <= max_pages:
-        url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=strawberry&_sacat=11700&LH&_pgn=" + str(page)
+        url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=strawberry&_sacat=11700&LH&_pgn=" + str(page) #Ändrar sida genom att sätta in page variabeln i webbadressen
         source_code = requests.get(url)
         down_img = source_code.text
         soup = BeautifulSoup(down_img, features="html.parser")
